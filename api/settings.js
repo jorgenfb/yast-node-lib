119a9c021c884f20cfde62dbd773eda50495933c
Jørgen Borgesen <joergen.borgesen@gmail.com>
2014-12-15 08:58:40 +0100

    fixed(settings) Settings are now transformed to an array.

--- api/settings.js
+++ api/settings.js
@@ -2,16 +2,17 @@ var api = require('./api');
 
 function list(credentials){
 	return api.request('user.getSettings', credentials).then(function(response){
-		// TODO: transform
-		return response;
+		var keys = response.keys;
+		var values = response.values;
+
+		return keys.map(function(key, i){
+			return {name: key, value: values[i]};
+		});
 	});
 }
 
 function update(credentials, settingName, settingValue){
-	return api.request('user.setSetting', {key: settingName, value: settingValue}, credentials).then(function(response){
-		// TODO: transform
-		return response;
-	});
+	return api.request('user.setSetting', {key: settingName, value: settingValue}, credentials);
 }
 
 module.exports = {
